f1e1bc2084aabcc4fd3312b812ac9c35da05dbbf
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-12-13 23:00:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import TechSkills from './Components/TechSkills';
-import {Card, Row, Col, Container, Image} from 'react-bootstrap';
 import Header from './Components/Header';
 import Resume from './Components/Resume';
 import "bootstrap/dist/css/bootstrap.css";
