001a1deabcaced89d5e822cd7a96c996eb540cb0
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-12-14 16:57:06 -0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,11 +6,10 @@ import {Card} from 'react-bootstrap';
 function Header() {
     return (
         <div>
-        <Card>
-            <h3>CARMEN KESHO</h3>
-            <h5><SiGmail/> miss.cdv@gmail.com</h5>
-            <h5><SiLinkedin/> https://www.linkedin.com/in/carmenvkesho/ </h5>
-            <h5><SiGithub /> https://github.com/CVKesho82</h5>
+        <Card className="Card">
+            <h1>CARMEN V KESHO</h1>
+            <h2><a href= "mailto:miss.cdv@gmail.com"><SiGmail/></a> miss.cdv@gmail.com</h2>
+            <h2><a href="https://www.linkedin.com/in/carmenvkesho/"><SiLinkedin/></a>  <a href="https://github.com/CVKesho82"><SiGithub/></a></h2>
             </Card>  
         </div>
     )
