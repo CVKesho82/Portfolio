cb7fb5568b6f155ff09f25616352da2941383c48
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-12-14 14:55:03 -0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import {SiGmail,SiLinkedin,SiGithub} from 'react-icons/si';
 import "bootstrap/dist/css/bootstrap.css";
+import {Card} from 'react-bootstrap';
 
 function Header() {
     return (
         <div>
+        <Card>
             <h3>CARMEN KESHO</h3>
             <h5><SiGmail/> miss.cdv@gmail.com</h5>
             <h5><SiLinkedin/> https://www.linkedin.com/in/carmenvkesho/ </h5>
             <h5><SiGithub /> https://github.com/CVKesho82</h5>
-            
+            </Card>  
         </div>
     )
 }
