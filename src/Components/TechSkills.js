5136e812e4f9718b5b84a2e6ec09d663da7d6686
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-12-13 23:04:36 -0500

    Update TechSkills.js

--- src/Components/TechSkills.js
+++ src/Components/TechSkills.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Card, Row, Col, Container, Image} from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.css";
 import {SiHtml5,SiCss3,SiBootstrap,SiPostgresql,SiSequelize,SiReact,SiJavascript,SiNodedotjs} from 'react-icons/si';
 
@@ -7,28 +6,19 @@ const TechSkills = () => {
   return (
     <div>
       <h3>Technical Skills</h3>
-      
-      {/* <Card>
-        <Card.Header><h5>Front End</h5></Card.Header>
-          <Card.Body> */}
             <h4>Front End</h4>
             <SiHtml5 />{"  "}
             <SiCss3 />{"  "}
             <SiBootstrap />{"  "}
             <SiReact />{"  "}
             <SiJavascript />{"  "}
-          {/* </Card.Body>{"  "}
-      </Card> */}
+          
       <br></br>
-      {/* <Card>
-        <Card.Header>Back End</Card.Header>
-          <Card.Body> */}
+     
           <h4>Back End</h4>
             <SiPostgresql />{"  "}
             <SiSequelize />{"  "}
             <SiNodedotjs />{"  "}
-          {/* </Card.Body> */}
-      {/* // </Card> */}
         
       
     </div>
