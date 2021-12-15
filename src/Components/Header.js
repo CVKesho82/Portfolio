6c5c9959cbdea9c926167c18511d129b2493c870
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-12-15 06:30:36 -0500

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,9 +8,17 @@ function Header() {
         <div>
         <Card className="Card">
             <h1>CARMEN V KESHO</h1>
-            <h2><a href= "mailto:miss.cdv@gmail.com"><SiGmail/></a> miss.cdv@gmail.com</h2>
-            <h2><a href="https://www.linkedin.com/in/carmenvkesho/"><SiLinkedin/></a>  <a href="https://github.com/CVKesho82"><SiGithub/></a></h2>
-            </Card>  
+            <h2><a href= "mailto:miss.cdv@gmail.com"><SiGmail/>miss.cdv@gmail.com</a>{"      "}<a href="https://www.linkedin.com/in/carmenvkesho/"><SiLinkedin/></a>{"       "}  <a href="https://github.com/CVKesho82"><SiGithub/></a> </h2>
+            
+            <h1>Technical Skills</h1>
+            <Card.Body>
+            <h4>Front End <SiHtml5 />{"  "}<SiCss3 />{"  "}<SiBootstrap />{"  "}
+            <SiReact />{"  "}<SiJavascript />{"  "}
+            </h4>
+            <h4>Back End <SiPostgresql />{"  "}<SiSequelize />{"  "}<SiNodedotjs />{"  "}</h4>        
+            
+          </Card.Body>
+        </Card>   
         </div>
     )
 }
