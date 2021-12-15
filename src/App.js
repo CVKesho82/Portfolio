b50c3a7f09b94b74709140111f9ae600ce514e6a
Carmen Kesho <85264996+CVKesho82@users.noreply.github.com>
2021-12-15 06:32:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import TechSkills from './Components/TechSkills';
 import Header from './Components/Header';
 import Resume from './Components/Resume';
 import "bootstrap/dist/css/bootstrap.css";
@@ -11,8 +10,6 @@ function App() {
       <header className="App-header">
         <p>
           <Header />
-          <br></br>
-          <TechSkills />
           <br></br>
           <Projects />
           <br></br>
